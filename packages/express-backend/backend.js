3c73eadacf4adba9063b1f1fedcbdb747d282049
Nicole Ruedger <ruedgern@gmail.com>
2024-02-08 12:33:54 -0800

    Added missing try/catch statements

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -26,19 +26,29 @@ app.get("/users", async (req, res) => {
 
 app.get("/users/:id", async (req, res) => {
   const id = req.params["id"];
-  const result = await userServices.findUserById(id);
-  if (result === undefined || result === null)
-    res.status(404).send("Resource not found.");
-  else {
-    res.send({ users_list: result });
+  try {
+    const result = await userServices.findUserById(id);
+    if (result === undefined || result === null)
+      res.status(404).send("Resource not found.");
+    else {
+      res.send({ users_list: result });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred in the server.")
   }
 });
 
 app.post("/users", async (req, res) => {
   const user = req.body;
-  const savedUser = await userServices.addUser(user);
-  if (savedUser) res.status(201).send(savedUser);
-  else res.status(500).end();
+  try {
+    const savedUser = await userServices.addUser(user);
+    if (savedUser) res.status(201).send(savedUser);
+    else res.status(500).end();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred in the server.")
+  }
 });
 
 app.delete("/users/:id", async (req, res) => {
